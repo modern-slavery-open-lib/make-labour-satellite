22cac5be8efae7139b834201348aa7b23d42b05e
Jacob <jacob@shrunk.ai>
2022-07-11 17:06:48 +1000

    Removed project files from repo

--- extract_ILO_data.py
+++ extract_ILO_data.py
@@ -30,6 +30,12 @@ assert country_col in df.columns
 year_col = 'Time'
 assert year_col in df.columns
 
+n_sectors = len(sector_labels)+1  # total value is final sector category
+
+# Check for duplicates
+count_recs = df.groupby(['Reference area', 'Time'])['Total'].count().reset_index(name='count')
+assert count_recs['count'].max() == 1
+
 # Unpack
 subs = []
 vals = []
@@ -38,12 +44,25 @@ for i, row in df.iterrows():
     v = row['Total']
 
     if isinstance(v, (int, float, np.number)) and np.isfinite(v) and v > 0:
+
         t = years.index(row[year_col])
         c = countries.index(row[country_col])
-        subs.append([t, c])
-        vals.append(v)
 
+        new_block_subs = [[t, c, n_sectors]]
+        new_block_vals = [v]
+
+        for j, s in enumerate(sector_labels):
+            sec_val = row[s]
+            if isinstance(sec_val, (int, float, np.number)) and np.isfinite(sec_val) and sec_val > 0:
+                new_block_subs.append([t, c, j])
+                new_block_vals.append(sec_val)
+
+        subs.extend(new_block_subs)
+        vals.extend(new_block_vals)
+
+# Tests
+assert len(subs) == len(vals)
 
 # Data store
-edges = [len(years), len(countries)]
+edges = [len(years), len(countries), n_sectors]
 store_spten = tf.SparseTensor(indices=subs, values=vals, dense_shape=edges)

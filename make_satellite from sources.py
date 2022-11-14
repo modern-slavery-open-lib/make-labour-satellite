34c49067f6403d20efce85548d5766c6c9206b38
jacob-shrunk-labs <jacob@shrunk.ai>
2022-11-14 14:51:54 +1100

    Wrote concordance tests

--- make_satellite from sources.py
+++ make_satellite from sources.py
@@ -1,11 +1,11 @@
 import numpy as np
-import pandas as pd
 from pathlib import Path
+from lib.preprocessing import read_aggregator
 
-current_working_directory = Path.cwd()
+current_working_directory = str(Path.cwd())
 
 root_country_legend = None
 
 source_root_map = np.random.rand(10, 6357)
-root_base_map = pd.read_csv(str(current_working_directory) + '/concs/HSCPC_Eora25_secagg.csv')
+root_base_map = read_aggregator(current_working_directory + '/concs/HSCPC_Eora25_secagg.csv')
 source_base_map = np.matmul(source_root_map, np.transpose(root_base_map))
---
+++ tests/map_tests.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+def conc_all_is_numeric(ar):
+    assert np.all(np.isfinite(ar))
+
+
+def conc_all_is_bool(ar):
+    assert ((ar == 0) | (ar == 1)).all()
+
+
+def conc_is_unique(ar):
+    marginal_sum = ar.sum(axis=0)
+    assert np.all(marginal_sum == 1)
+
+
+def conc_is_complete(ar):
+    marginal_sum = ar.sum(axis=1)
+    assert np.all(marginal_sum > 0)
